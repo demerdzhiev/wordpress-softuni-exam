ecae197c41bcb59812a0a2609fdb2cae3a600ddd
Dimitar Demerdzhiev <89989420+demerdzhiev@users.noreply.github.com>
2023-04-30 17:25:05 +0300

    Typo fixed

--- wp-content/themes/exam-homes/footer.php
+++ wp-content/themes/exam-homes/footer.php
@@ -1,22 +1,23 @@
 		
-		<footer class="site-footer">
-			<p>&copy; Copyright <?php echo date( 'Y' ) ?> 
-			</p>
-		</footer>
-	</div>
-    <?php wp_footer(); ?>
+<footer class="site-footer">
+	<p>&copy; Copyright <?php echo date( 'Y' ) ?> 
+	</p>
+</footer>
+</div>
 
-	<div class="footer-nav-menu">
-        <?php
-        if ( has_nav_menu( 'footer_menu' ) ) {
-                wp_nav_menu(
-                       array(
-                                'theme_location' => 'footer_menu',
-                        )
-               );
-        }
-        ?>
-    </div>
+<?php wp_footer(); ?>
+
+<div class="footer-nav-menu">
+    <?php
+    if ( has_nav_menu( 'footer_menu' ) ) {
+            wp_nav_menu(
+                   array(
+                            'theme_location' => 'footer_menu',
+                    )
+           );
+    }
+    ?>
+</div>
 
 </body>
 </html>
