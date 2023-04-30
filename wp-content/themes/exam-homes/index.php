0f997c3dd9df3078f237a587cab36b5bec3004eb
Dimitar Demerdzhiev <89989420+demerdzhiev@users.noreply.github.com>
2023-04-30 09:46:57 +0300

    Header and footer are updated

--- wp-content/themes/exam-homes/index.php
+++ wp-content/themes/exam-homes/index.php
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Properties</title>
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-
-	<!-- <link rel="stylesheet" href="./css/master.css"> -->
-	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
-    <?php wp_head(); ?>
-</head>
-<body>
-	<div class="site-wrapper">
-		<header class="site-header">
-			<h1 class="site-title"><a href="#">Properties Offers</a></h1>
-		</header>
-
+<?php get_header(); ?>
 		<ul class="properties-listing">
 			<li class="property-card">
 				<div class="property-primary">
@@ -56,15 +38,4 @@
 				</div>
 			</li>
 		</ul>
-		
-		<footer class="site-footer">
-			<p>&copy; Copyright 2023 | Developer links: 
-				<a href="/edits.html">Edits</a>,
-				<a href="/index.html">Home</a>,
-				<a href="/single.html">Single</a>
-			</p>
-		</footer>
-	</div>
-
-</body>
-</html>
+        <?php get_footer(); ?> 
