19ea90d865aea7f4b00681894e830a431a71f624
Dimitar Demerdzhiev <89989420+demerdzhiev@users.noreply.github.com>
2023-04-30 11:11:38 +0300

    Menu is added to footer

--- wp-content/themes/exam-homes/footer.php
+++ wp-content/themes/exam-homes/footer.php
@@ -5,5 +5,18 @@
 		</footer>
 	</div>
     <?php wp_footer(); ?>
+
+	<div class="footer-nav-menu">
+        <?php
+        if ( has_nav_menu( 'footer_menu' ) ) {
+                wp_nav_menu(
+                       array(
+                                'theme_location' => 'footer_menu',
+                        )
+               );
+        }
+        ?>
+    </div>
+
 </body>
 </html>
