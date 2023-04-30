76086c8b4360a566719efc48b54835eb0b9ba5de
Dimitar Demerdzhiev <89989420+demerdzhiev@users.noreply.github.com>
2023-04-30 09:46:35 +0300

    Menu function registered

--- wp-content/themes/exam-homes/functions.php
+++ wp-content/themes/exam-homes/functions.php
@@ -12,3 +12,21 @@ function exam_assets() {
     wp_enqueue_style( 'exam-homes', get_stylesheet_directory_uri() . '/css/master.css', array(), filemtime(  get_template_directory() . '/css/master.css' ) );
 }
 add_action( 'wp_enqueue_scripts', 'exam_assets' );
+
+
+
+
+
+
+/**
+ * Taking care of our custom menus
+ *
+ * @return void
+ */
+function exam_register_nav_menu(){
+    register_nav_menus( array(
+        'primary_menu' => __( 'Primary Menu Name', 'softuni' ),
+        'footer_menu'  => __( 'Footer Menu', 'softuni' ),
+    ) );
+}
+add_action( 'after_setup_theme', 'exam_register_nav_menu', 0 );
